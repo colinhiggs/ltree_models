c09361f6ff053af615e5cf46b7fe1d2761848e58
Colin Higgs <colin.higgs@ed.ac.uk>
2021-07-06 12:03:30 +0100

    break functionality into modules

---
+++ ltree/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy_utils import LtreeType, Ltree
+
+from sqlalchemy import (
+    Column,
+    Text,
+)
+
+class NodeBase:
+    def __repr__(self):
+       return f"{self.__class__.__name__}(id={self.id!r}, name={self.name!r}, path={self._path!r})"
+
+
+def class_factory(base, id_type, tablename = 'oltree_nodes'):
+    if not isinstance(id_type, Column):
+        id_type = Column(id_type, primary_key=True)
+    class_attrs = {
+        '__tablename__': tablename,
+        'id': id_type,
+        'name': Column(Text, nullable=False),
+        '_path': Column(LtreeType, nullable=False, unique=True),
+    }
+
+    LtreeNode = type('LtreeNode', (base, NodeBase), class_attrs)
+
+    return LtreeNode
