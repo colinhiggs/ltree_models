5718bda1c57cec78a09bf25af44776d0d66df929
Colin Higgs <colin.higgs@ed.ac.uk>
2021-07-16 14:51:43 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+requires = [
+    'SQLAlchemy>=1.4',
+]
+
+tests_require = [
+    'testing.postgresql',
+    'tox',
+]
+
+setup_requires = [
+    'setuptools-git-versioning',
+]
+
+setup(
+    name = 'ltree',
+    version_config = True,
+    packages = find_packages(),
+    setup_requires = setup_requires,
+    install_requires = requires,
+    extras_require = {
+        'testing': tests_require,
+    },
+    description = 'sqlalchemy models for ltree.',
+    long_description=README,
+    author = 'Colin Higgs',
+    author_email = 'colin.higgs70@gmail.com',
+    license = 'GNU Affero General Public License v3 or later (AGPLv3+)',
+    url = '',
+    keywords = ['json', 'api', 'json-api', 'jsonapi', 'jsonschema', 'openapi', 'pyramid', 'sqlalchemy'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python :: 3',
+    ],
+)
