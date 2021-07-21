063a3c35df604cf3252a6101e846c13d938aa149
Colin Higgs <colin.higgs@ed.ac.uk>
2021-07-21 22:44:51 +0100

    working example

--- readme1.py
+++ readme1.py
@@ -1,6 +1,8 @@
 import ltree
+import testing.postgresql
 from sqlalchemy import (
   Column,
+  create_engine,
   Integer,
   Text,
 )
@@ -21,3 +23,13 @@ class OrderedNode(Base, ltree.OLtreeMixin):
     __tablename__ = 'oltree_nodes'
     id = Column(Integer, primary_key=True)
     name = Column(Text, nullable=False)
+
+# Create a new postgresql database in /tmp
+db = testing.postgresql.Postgresql()
+engine = create_engine(db.url(), echo=False)
+ltree.add_ltree_extension(engine)
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+unordered_builder = ltree.LtreeBuilder(engine, UnorderedNode)
+unordered_builder.populate(2, 3, unordered_builder.path_chooser_sequential)
+unordered_builder.print_tree()
