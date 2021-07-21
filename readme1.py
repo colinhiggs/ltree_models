192ba4cec8af72e4c121c7f87a6eaa5125cb3e6a
Colin Higgs <colin.higgs@ed.ac.uk>
2021-07-21 22:13:44 +0100

    starting some docs

--- readme1.py
+++ readme1.py
@@ -1,18 +1,17 @@
-# SQLAlchemy Ltree Model Mixins
-
-## Synopsis
-
-```python
 import ltree
 from sqlalchemy import (
   Column,
   Integer,
   Text,
 )
+from sqlalchemy.ext.declarative import (
+    declarative_base
+)
 from sqlalchemy.orm import (
     Session,
 )
 
+Base = declarative_base()
 class UnorderedNode(Base, ltree.LtreeMixin):
     __tablename__ = 'ltree_nodes'
     id = Column(Integer, primary_key=True)
@@ -22,4 +21,3 @@ class OrderedNode(Base, ltree.OLtreeMixin):
     __tablename__ = 'oltree_nodes'
     id = Column(Integer, primary_key=True)
     name = Column(Text, nullable=False)
-```
